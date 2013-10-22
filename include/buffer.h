c93c8cc69b7c0a3129de48e6633bce989c52607f
Rafał Wojdyła <omeg@invisiblethingslab.com>
2013-10-22 04:24:04 +0200

    buffer: support for multiple buffers

--- include/buffer.h
+++ include/buffer.h
@@ -4,6 +4,9 @@
 // Circular memory queue implementation. 
 // All int-returning functions return 0 on success, <0 on failure.
 
+struct _buffer;
+typedef struct _buffer buffer_t;
+
 // Underflow mode for read operations.
 typedef enum
 {
@@ -11,20 +14,20 @@ typedef enum
 	BUFFER_NO_UNDERFLOW
 } BUFFER_UNDERFLOW_MODE;
 
-// allocate memory and initialize
-int buffer_create(size_t buffer_size);
+// allocate memory and initialize, return 0 = failure
+buffer_t *buffer_create(size_t buffer_size);
 
 // free memory and deinitialize
-int buffer_destroy(void);
+int buffer_destroy(buffer_t *buffer);
 
 // "push" data to the buffer
-int buffer_add_data(void *data, size_t data_size);
+int buffer_add_data(buffer_t *buffer, void *data, size_t data_size);
 
 // "pop" data from the buffer, size=0: get all data (make sure you have the space for it)
-int buffer_get_data(void *data, size_t *data_size, BUFFER_UNDERFLOW_MODE underflow_mode);
+int buffer_get_data(buffer_t *buffer, void *data, size_t *data_size, BUFFER_UNDERFLOW_MODE underflow_mode);
 
 // returns bytes used by data
-size_t buffer_get_size(void);
+size_t buffer_get_size(buffer_t *buffer);
 
 // zero-fill buffer, rewind internal pointer
-int buffer_clear(void);
+int buffer_clear(buffer_t *buffer);
--- shared/buffer.c
+++ shared/buffer.c
@@ -1,137 +1,130 @@
 #include "buffer.h"
 #include "log.h"
 
-static size_t g_buffer_size = 0;  // buffer size
-static char  *g_buffer_start = 0; // start of storage memory
-static char  *g_data_start = 0;   // pointer to the first byte of data. null = buffer empty
-static char  *g_data_end = 0;     // pointer to the first free byte. null = buffer full
+struct _buffer
+{
+	size_t size;       // buffer size
+	char  *start;      // start of storage memory
+	char  *data_start; // pointer to the first byte of data. null = buffer empty
+	char  *data_end;   // pointer to the first free byte. null = buffer full
+};
 
 // allocate memory and set variables
-int buffer_create(size_t buffer_size)
+buffer_t *buffer_create(size_t buffer_size)
 {
+	buffer_t *buffer = NULL;
 	debugf("buffer_create: %d bytes\n", buffer_size);
-
-	if (g_buffer_start != 0) // already allocated
-	{
-		errorf("buffer_create: already allocated\n");
-		return -1;
-	}
 
 	if (buffer_size == 0)
 	{
 		errorf("buffer_create: size == 0\n");
-		return -1;
+		return NULL;
 	}
 
-	g_buffer_size = buffer_size;
-	g_buffer_start = (char*) malloc(g_buffer_size);
-	g_data_start = g_data_end = 0;
-	if (g_buffer_start == 0)
+	buffer = (buffer_t*) malloc(sizeof(buffer_t));
+	if (!buffer)
 	{
-		errorf("buffer_create: no memory\n");
-		return -1;
+		errorf("buffer_create: out of memory\n");
+		return NULL;
 	}
 
-	return 0;
+	buffer->size = buffer_size;
+	buffer->start = (char*) malloc(buffer->size);
+	buffer->data_start = buffer->data_end = 0;
+	if (buffer->start == 0)
+	{
+		free(buffer);
+		errorf("buffer_create: no memory\n");
+		return NULL;
+	}
+
+	return buffer;
 }
 
 // free memory and deinitialize
-int buffer_destroy(void)
+int buffer_destroy(buffer_t *buffer)
 {
-	debugf("buffer_destroy\n");
+	debugf("buffer_destroy(0x%0x)\n", buffer);
 
-	if (g_buffer_start == 0) // not allocated
-	{
-		errorf("buffer_destroy: not allocated\n");
-		return -1;
-	}
-
-	free(g_buffer_start);
-	g_data_start = g_data_end = g_buffer_start = 0;
-	g_buffer_size = 0;
-
+	free(buffer->start);
+	free(buffer);
+	
 	return 0;
 }
 
 // zero-fill buffer, rewind internal pointer
-int buffer_clear(void)
+int buffer_clear(buffer_t *buffer)
 {
-	debugf("buffer_clear\n");
+	debugf("buffer_clear(0x%x)\n", buffer);
 
-	if (g_buffer_start == 0) // not allocated
-	{
-		errorf("buffer_clear: not allocated\n");
-		return -1;
-	}
-
-	g_data_start = g_data_end = 0;
-	memset(g_buffer_start, 0, g_buffer_size);
+	buffer->data_start = buffer->data_end = 0;
+	memset(buffer->start, 0, buffer->size);
 
 	return 0;
 }
 
 // queue data to the buffer
-int buffer_add_data(void *pdata, size_t data_size)
+int buffer_add_data(buffer_t *buffer, void *pdata, size_t data_size)
 {
 	char *data = (char*) pdata;
 	size_t used_size, free_size, free_at_end;
 
-	debugf("buffer_add_data(0x%x, %d)\n", data, data_size);
+	debugf("buffer_add_data(0x%x, 0x%x, %d)\n", buffer, data, data_size);
 
-	if (g_data_end == 0 && g_data_start != 0)  // buffer full
+	if (buffer->data_end == 0 && buffer->data_start != 0)  // buffer full
 	{
-		errorf("buffer_add_data(0x%x, %d): buffer full\n", data, data_size);
+		errorf("buffer_add_data(0x%x, 0x%x, %d): buffer full\n", buffer, data, data_size);
 		return -1;
 	}
 
 	if (data_size == 0)
 		return 0;
 
-	if (g_data_start == 0) // empty, initialize pointers
+	if (buffer->data_start == 0) // empty, initialize pointers
 	{
-		g_data_start = g_data_end = g_buffer_start;
+		buffer->data_start = buffer->data_end = buffer->start;
 	}
 
 	// needs that if DataEnd is at the end of buffer it's not wrapped to 0
-	if (g_data_start <= g_data_end) // data in one consecutive block (or empty)
-		used_size = (g_data_end - g_data_start);
+	if (buffer->data_start <= buffer->data_end) // data in one consecutive block (or empty)
+		used_size = (buffer->data_end - buffer->data_start);
 	else
-		used_size = (g_data_end + g_buffer_size - g_data_start);
+		used_size = (buffer->data_end + buffer->size - buffer->data_start);
 
-	free_size = g_buffer_size - used_size;
+	free_size = buffer->size - used_size;
 	//  =       <       <       <!      >
 	// [.....] [oo...] [.oo..] [...oo] [o...o]
 	if (data_size > free_size)
 	{
-		errorf("buffer_add_data(0x%x, %d): buffer too small (free: %d)\n", data, data_size, free_size);
+		errorf("buffer_add_data(0x%x, 0x%x, %d): buffer too small (free: %d)\n", buffer, data, data_size, free_size);
 		return -1;
 	}
 
-	if (g_data_start > g_buffer_start && g_data_end < g_buffer_start+g_buffer_size) // [.oo..]
+	if (buffer->data_start > buffer->start && buffer->data_end < buffer->start+buffer->size) // [.oo..]
 	{
-		free_at_end = (g_buffer_start + g_buffer_size - g_data_end);
+		free_at_end = (buffer->start + buffer->size - buffer->data_end);
 		if (data_size <= free_at_end)
-			memcpy(g_data_end, data, data_size);
+			memcpy(buffer->data_end, data, data_size);
 		else
 		{
-			memcpy(g_data_end, data, data_size-free_at_end);
-			memcpy(g_buffer_start, (data+data_size-free_at_end), free_at_end);
+			memcpy(buffer->data_end, data, data_size-free_at_end);
+			memcpy(buffer->start, (data+data_size-free_at_end), free_at_end);
 		}
 	}
 	else
 	{
-		if (g_data_end == g_buffer_start+g_buffer_size) // end at the limit, wrap to 0
-			g_data_end = g_buffer_start;
+		if (buffer->data_end == buffer->start+buffer->size) // end at the limit, wrap to 0
+			buffer->data_end = buffer->start;
 		// now we have one consecutive block of free memory so just copy
-		memcpy(g_data_end, data, data_size);
+		memcpy(buffer->data_end, data, data_size);
 	}
 
-	g_data_end += data_size;
-	if (g_data_end > g_buffer_start+g_buffer_size) // wrap only if end > limit, if end==limit pointer stays at the end
-		g_data_end -= g_buffer_size;
+	buffer->data_end += data_size;
+	if (buffer->data_end > buffer->start+buffer->size) // wrap only if end > limit, if end==limit pointer stays at the end
+		buffer->data_end -= buffer->size;
 
-	if (used_size + data_size == g_buffer_size) // buffer full
-		g_data_end = 0;
+	if (used_size + data_size == buffer->size) // buffer full
+		buffer->data_end = 0;
 
 	return 0;
 }
@@ -139,13 +132,13 @@ int buffer_add_data(void *pdata, size_t data_size)
 // "pop" data from the buffer
 // underflow = true: don't treat underflow as errors (reading empty buffer is ok etc)
 // data_size = 0: read all (use carefully in case of buffer overruns)
-int buffer_get_data(void *pdata, size_t *data_size, BUFFER_UNDERFLOW_MODE underflow_mode)
+int buffer_get_data(buffer_t *buffer, void *pdata, size_t *data_size, BUFFER_UNDERFLOW_MODE underflow_mode)
 {
 	char *data = (char*) pdata;
 	size_t used_size, used_at_end;
 
-	errorf("buffer_get_data(0x%x, 0x%x, %d)\n", data, data_size, underflow_mode);
-	if (g_data_start == 0)  // buffer empty
+	errorf("buffer_get_data(0x%x, 0x%x, 0x%x, %d)\n", buffer, data, data_size, underflow_mode);
+	if (buffer->data_start == 0)  // buffer empty
 	{
 		*data_size = 0;
 		if ((underflow_mode==BUFFER_ALLOW_UNDERFLOW) || (*data_size == 0))
@@ -157,18 +150,18 @@ int buffer_get_data(void *pdata, size_t *data_size, BUFFER_UNDERFLOW_MODE underf
 		}
 	}
 
-	if (g_data_end == 0) // buffer full, only _pDataStart is valid
+	if (buffer->data_end == 0) // buffer full, only _pDataStart is valid
 	{
-		g_data_end = g_data_start;
-		used_size = g_buffer_size;
+		buffer->data_end = buffer->data_start;
+		used_size = buffer->size;
 	}
 	else
 	{
 		// needs that if DataEnd is at the end of buffer it's not wrapped to 0
-		if (g_data_start < g_data_end) // data in one consecutive block
-			used_size = (g_data_end - g_data_start);
+		if (buffer->data_start < buffer->data_end) // data in one consecutive block
+			used_size = (buffer->data_end - buffer->data_start);
 		else
-			used_size = (g_data_end + g_buffer_size - g_data_start);
+			used_size = (buffer->data_end + buffer->size - buffer->data_start);
 	}
 
 	//  full    <       >       <       <
@@ -191,46 +184,46 @@ int buffer_get_data(void *pdata, size_t *data_size, BUFFER_UNDERFLOW_MODE underf
 		}
 	}
 
-	if (g_data_start >= g_data_end) // [o..oo] should be safe for full buffer too (_pDataEnd == _pDataStart)
+	if (buffer->data_start >= buffer->data_end) // [o..oo] should be safe for full buffer too (_pDataEnd == _pDataStart)
 	{
-		used_at_end = (g_buffer_start + g_buffer_size - g_data_start);
+		used_at_end = (buffer->start + buffer->size - buffer->data_start);
 		if (*data_size <= used_at_end)
-			memcpy(data, g_data_start, *data_size);
+			memcpy(data, buffer->data_start, *data_size);
 		else
 		{
-			memcpy(data, g_data_start, used_at_end);
-			memcpy((data+used_at_end), g_buffer_start, *data_size-used_at_end);
+			memcpy(data, buffer->data_start, used_at_end);
+			memcpy((data+used_at_end), buffer->start, *data_size-used_at_end);
 		}
 	}
 	else
 	{
 		// now we have one consecutive block of data memory so just copy
-		memcpy(data, g_data_start, *data_size);
+		memcpy(data, buffer->data_start, *data_size);
 	}
 
-	g_data_start += *data_size;
-	if (g_data_start >= g_buffer_start+g_buffer_size)
-		g_data_start -= g_buffer_size;
+	buffer->data_start += *data_size;
+	if (buffer->data_start >= buffer->start+buffer->size)
+		buffer->data_start -= buffer->size;
 
 	if (used_size - *data_size == 0) // buffer empty
 	{
-		g_data_start = 0;
+		buffer->data_start = 0;
 	}
 
 	return 0;
 }
 
 // get used data size
-size_t buffer_get_size(void)
+size_t buffer_get_size(buffer_t *buffer)
 {
-	if (g_buffer_size == 0 || g_buffer_start == 0 || g_data_start == 0)
+	if (buffer->size == 0 || buffer->start == 0 || buffer->data_start == 0)
 		return 0;
 
-	if (g_data_end == 0)  // full
-		return g_buffer_size;
+	if (buffer->data_end == 0)  // full
+		return buffer->size;
 
-	if (g_data_start < g_data_end) // data in one consecutive block
-		return (g_data_end - g_data_start);
+	if (buffer->data_start < buffer->data_end) // data in one consecutive block
+		return (buffer->data_end - buffer->data_start);
 	else
-		return (g_data_end + g_buffer_size - g_data_start);
+		return (buffer->data_end + buffer->size - buffer->data_start);
 }
