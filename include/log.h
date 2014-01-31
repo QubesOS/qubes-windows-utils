59a7d11b87c2802fa659a4851445ef2c90103c2f
Rafał Wojdyła <omeg@invisiblethingslab.com>
2014-01-31 07:13:13 +0100

    logger: merged changes from master branch

--- include/log.h
+++ include/log.h
@@ -16,35 +16,46 @@ void log_init(TCHAR *log_dir, TCHAR *base_name);
 // If logfile_path is NULL, use stderr.
 void log_start(TCHAR *logfile_path);
 
-void _logf(BOOL echo_to_stderr, TCHAR *format, ...);
+void _logf(BOOL echo_to_stderr, BOOL raw, const char *function_name, TCHAR *format, ...);
+// *_raw functions omit the timestamp, function name prefix and don't append newlines automatically.
+
+// Microsoft compilers define __FUNCTION__ as a string literal.
+// This is better than the C standard which defines __func__ as a char[] variable,
+// but we need this to compile with GCC...
 
 // Helper to not need to stick TEXT everywhere...
 #ifdef LOG_TO_STDERR
-#define logf(format, ...) _logf(TRUE, TEXT(format), ##__VA_ARGS__)
+#define logf(format, ...) _logf(TRUE, FALSE, __FUNCTION__, TEXT(format), ##__VA_ARGS__)
+#define logf_raw(format, ...) _logf(TRUE, TRUE, NULL, TEXT(format), ##__VA_ARGS__)
 #else
-#define logf(format, ...) _logf(FALSE, TEXT(format), ##__VA_ARGS__)
+#define logf(format, ...) _logf(FALSE, FALSE, __FUNCTION__, TEXT(format), ##__VA_ARGS__)
+#define logf_raw(format, ...) _logf(FALSE, TRUE, NULL, TEXT(format), ##__VA_ARGS__)
 #endif
 
 // debugf never echoes to stderr (if logging to file).
 #if defined(DEBUG) || defined(_DEBUG)
-#define debugf(format, ...) _logf(FALSE, TEXT(format), ##__VA_ARGS__) 
+#define debugf(format, ...) _logf(FALSE, FALSE, __FUNCTION__, TEXT(format), ##__VA_ARGS__)
+#define debugf_raw(format, ...) _logf(FALSE, TRUE, NULL, TEXT(format), ##__VA_ARGS__)
 #else
 #define debugf(format, ...)
+#define debugf_raw(format, ...)
 #endif
 
 // errorf always echoes to stderr.
-#define errorf(format, ...) _logf(TRUE, TEXT(format), ##__VA_ARGS__) 
+#define errorf(format, ...) _logf(TRUE, FALSE, __FUNCTION__, TEXT(format), ##__VA_ARGS__)
+#define errorf_raw(format, ...) _logf(TRUE, TRUE, NULL, TEXT(format), ##__VA_ARGS__)
 
-void _perror(TCHAR *prefix);
-#define perror(prefix) _perror(TEXT(prefix))
+void _perror(const char *function_name, TCHAR *prefix);
+#define perror(prefix) _perror(__FUNCTION__, TEXT(prefix))
 
 // hex_dump only logs if DEBUG is defined.
 // You can define LOG_NO_HEX_DUMP to disable it even in DEBUG build (it can generate massive log files).
 void _hex_dump (TCHAR *desc, void *addr, int len);
 #if (defined(DEBUG) || defined(_DEBUG)) && !defined(LOG_NO_HEX_DUMP)
-#define hex_dump(desc, addr, len) _hex_dump(desc, addr, len)
+#define hex_dump(desc, addr, len) _hex_dump(TEXT(desc), addr, len)
 #else
 #define hex_dump(desc, addr, len)
 #endif
 
+// Flush pending data to the log file.
 void log_flush();
