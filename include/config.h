3f1def3d59aac61a9333e5a5af6db06ef1c01e5f
Rafał Wojdyła <omeg@invisiblethingslab.com>
2014-07-07 21:49:03 +0200

    added CfgReadDword()

--- include/config.h
+++ include/config.h
@@ -8,3 +8,6 @@
 
 // Read a string value from registry config.
 DWORD CfgReadString(IN PWCHAR valueName, OUT PWCHAR value, IN DWORD valueLength);
+
+// Read a DWORD value from registry config.
+DWORD CfgReadDword(IN PWCHAR valueName, OUT PDWORD value);
--- shared/config.c
+++ shared/config.c
@@ -4,31 +4,62 @@
 // Read a string value from registry config.
 DWORD CfgReadString(IN PWCHAR valueName, OUT PWCHAR value, IN DWORD valueLength)
 {
-	HKEY key = NULL;
-	DWORD status;
-	DWORD type;
-	DWORD size;
+    HKEY key = NULL;
+    DWORD status;
+    DWORD type;
+    DWORD size;
 
-	SetLastError(status = RegOpenKey(HKEY_LOCAL_MACHINE, REG_CONFIG_KEY, &key));
-	if (status != ERROR_SUCCESS)
-		return status;
+    SetLastError(status = RegOpenKey(HKEY_LOCAL_MACHINE, REG_CONFIG_KEY, &key));
+    if (status != ERROR_SUCCESS)
+        return status;
 
-	size = sizeof(WCHAR) * (valueLength-1);
-	ZeroMemory(value, sizeof(WCHAR)*valueLength);
-	
-	SetLastError(status = RegQueryValueEx(key, valueName, NULL, &type, (PBYTE)value, &size));
-	if (status != ERROR_SUCCESS)
-		goto cleanup;
+    size = sizeof(WCHAR) * (valueLength-1);
+    ZeroMemory(value, sizeof(WCHAR)*valueLength);
 
-	if (type != REG_SZ)
-	{
-		status = ERROR_DATATYPE_MISMATCH;
-		goto cleanup;
-	}
+    SetLastError(status = RegQueryValueEx(key, valueName, NULL, &type, (PBYTE)value, &size));
+    if (status != ERROR_SUCCESS)
+        goto cleanup;
+
+    if (type != REG_SZ)
+    {
+        status = ERROR_DATATYPE_MISMATCH;
+        goto cleanup;
+    }
 
 cleanup:
-	if (key)
-		RegCloseKey(key);
+    if (key)
+        RegCloseKey(key);
 
-	return status;
+    return status;
+}
+
+// Read a DWORD value from registry config.
+DWORD CfgReadDword(IN PWCHAR valueName, OUT PDWORD value)
+{
+    HKEY key = NULL;
+    DWORD status;
+    DWORD type;
+    DWORD size;
+
+    SetLastError(status = RegOpenKey(HKEY_LOCAL_MACHINE, REG_CONFIG_KEY, &key));
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    size = sizeof(DWORD);
+
+    SetLastError(status = RegQueryValueEx(key, valueName, NULL, &type, (PBYTE)value, &size));
+    if (status != ERROR_SUCCESS)
+        goto cleanup;
+
+    if (type != REG_DWORD)
+    {
+        status = ERROR_DATATYPE_MISMATCH;
+        goto cleanup;
+    }
+
+cleanup:
+    if (key)
+        RegCloseKey(key);
+
+    return status;
 }
