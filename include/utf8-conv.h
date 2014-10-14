103837848fea1ef2e6d2a28d705745fd765d990c
Rafał Wojdyła <omeg@invisiblethingslab.com>
2014-10-14 23:41:03 +0200

    code cleanup: annotations, const, names

--- include/utf8-conv.h
+++ include/utf8-conv.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /* Returns number of characters in output buffer, without terminating NULL. */
-DWORD ConvertUTF8ToUTF16(char *input_utf8, WCHAR **output_utf16, size_t *output_len);
+DWORD ConvertUTF8ToUTF16(IN const char *inputUtf8, OUT WCHAR **outputUtf16, OUT size_t *cchOutput OPTIONAL);
 
 /* Returns number of characters in output buffer, without terminating NULL. */
-DWORD ConvertUTF16ToUTF8(WCHAR *pwszUtf16, char **ppszUtf8, size_t *pcbUtf8);
+DWORD ConvertUTF16ToUTF8(IN const WCHAR *inputUtf16, OUT char **outputUtf8, OUT size_t *cchOutput OPTIONAL);
--- shared/utf8-conv.c
+++ shared/utf8-conv.c
@@ -2,74 +2,78 @@
 #include <stdlib.h>
 #include <strsafe.h>
 
-DWORD ConvertUTF8ToUTF16(char *input_utf8, WCHAR **output_utf16, size_t *output_len)
+DWORD ConvertUTF8ToUTF16(IN const char *inputUtf8, OUT WCHAR **outputUtf16, OUT size_t *cchOutput OPTIONAL)
 {
     int result;
-    size_t len_utf8;
-    int len_utf16;
-    WCHAR *buf_utf16;
+    size_t cbUtf8;
+    int cchUtf16;
+    WCHAR *bufferUtf16;
 
-    if (FAILED(StringCchLengthA(input_utf8, STRSAFE_MAX_CCH, &len_utf8)))
+    if (FAILED(StringCchLengthA(inputUtf8, STRSAFE_MAX_CCH, &cbUtf8)))
         return GetLastError();
 
-    if (len_utf8 - 1 >= INT_MAX)
+    if (cbUtf8 - 1 >= INT_MAX)
         return ERROR_BAD_ARGUMENTS;
 
-    len_utf16 = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, input_utf8, (int) len_utf8 + 1, NULL, 0);
-    if (!len_utf16)
+    cchUtf16 = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, inputUtf8, (int) cbUtf8 + 1, NULL, 0);
+    if (!cchUtf16)
     {
         return GetLastError();
     }
 
-    buf_utf16 = (WCHAR*) malloc(len_utf16 * sizeof(WCHAR));
-    if (!buf_utf16)
+    bufferUtf16 = (WCHAR*) malloc(cchUtf16 * sizeof(WCHAR));
+    if (!bufferUtf16)
         return ERROR_NOT_ENOUGH_MEMORY;
 
-    result = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, input_utf8, (int) len_utf8 + 1, buf_utf16, len_utf16);
+    result = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, inputUtf8, (int) cbUtf8 + 1, bufferUtf16, cchUtf16);
     if (!result)
     {
-        free(buf_utf16);
+        free(bufferUtf16);
         return GetLastError();
     }
 
-    buf_utf16[len_utf16 - 1] = L'\0';
-    *output_utf16 = buf_utf16;
-    if (output_len)
-        *output_len = len_utf16 - 1; /* without terminating NULL character */
+    bufferUtf16[cchUtf16 - 1] = L'\0';
+    *outputUtf16 = bufferUtf16;
+    if (cchOutput)
+        *cchOutput = cchUtf16 - 1; /* without terminating NULL character */
 
     return ERROR_SUCCESS;
 }
 
-DWORD ConvertUTF16ToUTF8(WCHAR *input_utf16, char **output_utf8, size_t *output_len)
+DWORD ConvertUTF16ToUTF8(IN const WCHAR *inputUtf16, OUT char **outputUtf8, OUT size_t *cchOutput OPTIONAL)
 {
-    char *but_utf8;
-    int len_utf8;
-    DWORD conversion_flags = 0;
+    char *bufferUtf8;
+    int cbUtf8;
+    DWORD conversionFlags = 0;
 
     // WC_ERR_INVALID_CHARS is defined for Vista and later only
 #if (WINVER >= 0x0600)
-    conversion_flags = WC_ERR_INVALID_CHARS;
+    conversionFlags = WC_ERR_INVALID_CHARS;
 #endif
 
     /* convert filename from UTF-16 to UTF-8 */
     /* calculate required size */
-    len_utf8 = WideCharToMultiByte(CP_UTF8, conversion_flags, input_utf16, -1, NULL, 0, NULL, NULL);
-    if (!len_utf8)
+    cbUtf8 = WideCharToMultiByte(CP_UTF8, conversionFlags, inputUtf16, -1, NULL, 0, NULL, NULL);
+
+    if (!cbUtf8)
     {
         return GetLastError();
     }
-    but_utf8 = (char*) malloc(sizeof(PUCHAR)*len_utf8);
-    if (!but_utf8)
+
+    bufferUtf8 = (char*) malloc(cbUtf8);
+    if (!bufferUtf8)
     {
         return ERROR_NOT_ENOUGH_MEMORY;
     }
-    if (!WideCharToMultiByte(CP_UTF8, conversion_flags, input_utf16, -1, but_utf8, len_utf8, NULL, NULL))
+
+    if (!WideCharToMultiByte(CP_UTF8, conversionFlags, inputUtf16, -1, bufferUtf8, cbUtf8, NULL, NULL))
     {
-        free(but_utf8);
+        free(bufferUtf8);
         return GetLastError();
     }
-    if (output_len)
-        *output_len = len_utf8 - 1; /* without terminating NULL character */
-    *output_utf8 = but_utf8;
+
+    if (cchOutput)
+        *cchOutput = cbUtf8 - 1; /* without terminating NULL character */
+    *outputUtf8 = bufferUtf8;
     return ERROR_SUCCESS;
 }
